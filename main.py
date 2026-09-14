278e3037bb3dc296f3571e19f168aad3a3651e38
HenricoDeSistemas <henricoiswilliam@gmail.com>
2026-09-14 15:48:54 +0000

    Etapa 2 Final REAL

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ print(solucoes_ativas)
 
 #Etapa 2
 
+
 bancada = [
     [1,0],
     [0,1]
@@ -17,6 +18,7 @@ print("Status da Bancada N1"), bancada[0][0]
 print("Status da Bancada N2", bancada[0][1])
 print("Status da Bancada S1", bancada[0][0])
 print("Status da Bancada S2", bancada[0][1])
+
 
 with open("custos_cloud.csv", "r", encoding="utf-8") as arquivo:
     cabecalho = arquivo.readline()
@@ -37,7 +39,6 @@ custo_3 = float(linha_3.split(",")[1].strip())
 custo_4 = float(linha_4.split(",")[1].strip())
 total = custo_1 + custo_2 + custo_3 + custo_4
 
-print("\nPAINEL FINAL")
 print("Startup:", startup
       ["nome"])
 print("Bancada alocada: Bancada N1")
