e1406562c58781301b55ffa4c8a9d48e5e41d1e2
HenricoDeSistemas <henricoiswilliam@gmail.com>
2026-09-14 12:42:15 +0000

    Etapa 1

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+startup = {"nome": "CyberPulse Tech",
+            "segmento": "Segurança da Informação",
+              "ano_adesao":"2026",
+              }
+solucoes_ativas = ["Firewall IA", "Scan de Vulnerabilidades"]
 
+print(startup)
+print(solucoes_ativas)
